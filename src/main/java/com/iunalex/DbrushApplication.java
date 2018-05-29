6a4fc41839b07b5d8c111403423e31e5b7578d63
Alexandr Cusnir <Alexandr.Cusnir@endava.com>
2018-05-30 00:53:18 +0300

    Project init

---
+++ src/main/java/com/iunalex/DbrushApplication.java
@@ -0,0 +1,12 @@
+package com.iunalex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbrushApplication.class, args);
+	}
+}
